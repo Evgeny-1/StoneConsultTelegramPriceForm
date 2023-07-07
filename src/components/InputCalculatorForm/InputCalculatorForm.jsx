c470784f9ae190d4eb2987aac3843cb02271ec12
Evgeny Larin <Larinevgenij@gmail.com>
2023-07-07 14:56:07 +0300

    init

--- src/components/InputCalculatorForm/InputCalculatorForm.jsx
+++ src/components/InputCalculatorForm/InputCalculatorForm.jsx
@@ -3,7 +3,7 @@ import "./InputCalculatorForm.css"
 const InputCalculatorForm = (props) => {
     return(
         <div className="InputCalculatorForm">
-            <label>Курс ЦБ</label>
+            {/*<label>Курс ЦБ</label>*/}
             <input placeholder={props.placeholder}></input>
         </div>
     )
