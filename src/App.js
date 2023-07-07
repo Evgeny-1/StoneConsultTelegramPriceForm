d2e24e9e3be07ad11b93e1898592fdf1b2305e4e
Evgeny Larin <Larinevgenij@gmail.com>
2023-07-07 14:47:13 +0300

    init

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
+import CalculatorForm from "./components/InputCalculatorForm";
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
@@ -16,10 +17,53 @@ function App() {
   return (
       <div className="App">
         <Header />
-        <Routes>
-          <Route index element={<ProductList />}/>
-          <Route path={'form'} element={<Form />}/>
-        </Routes>
+
+        <CalculatorForm placeholder = "Курс ЦБ"/>
+
+        <CalculatorForm placeholder = "Валюта($/€)"/>
+        <CalculatorForm placeholder = "Страна"/>
+        <CalculatorForm placeholder = "Цена завода за м2"/>
+
+        <CalculatorForm placeholder = "Ширина"/>
+        <CalculatorForm placeholder = "Длина"/>
+        <CalculatorForm placeholder = "Толщина"/>
+
+        <CalculatorForm placeholder = "Плотность"/>
+
+        <CalculatorForm placeholder = "Объем"/>
+        <CalculatorForm placeholder = "Тоннаж"/>
+        <CalculatorForm placeholder = "Вместимость контейнера"/>
+
+        <CalculatorForm placeholder = "Фрахт"/>
+        <CalculatorForm placeholder = "Прочее"/>
+        <CalculatorForm placeholder = "Пошлина"/>
+
+        <CalculatorForm placeholder = "НДС до РФ"/>
+
+        <CalculatorForm placeholder = "Банковские расходы"/>
+
+        <CalculatorForm placeholder = "Фрахт без НДС"/>
+        <CalculatorForm placeholder = "Прочее без НДС"/>
+        <CalculatorForm placeholder = "Фрахт с НДС"/>
+        <CalculatorForm placeholder = "Прочее с НДС"/>
+
+        <CalculatorForm placeholder = "НДС по РФ"/>
+
+        <CalculatorForm placeholder = "Наценка"/>
+
+        <CalculatorForm placeholder = "Наименование камня($/€)"/>
+
+        <CalculatorForm placeholder = "Цена за м2($/€)"/>
+          <CalculatorForm placeholder = "Цена за м2(руб)"/>
+          <CalculatorForm placeholder = "Цена за шт($/€)"/>
+          <CalculatorForm placeholder = "Цена за шт(руб)"/>
+
+        {/*<Routes>*/}
+
+        {/*  <Route index element={<ProductList />}/>*/}
+        {/* <Route path={'form'} element={<Form />}/>*/}
+
+        {/*</Routes>*/}
       </div>
   );
 }
---
+++ src/components/InputCalculatorForm/InputCalculatorForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./InputCalculatorForm.css"
+const InputCalculatorForm = (props) => {
+    return(
+        <div className="InputCalculatorForm">
+            <label>Курс ЦБ</label>
+            <input placeholder={props.placeholder}></input>
+        </div>
+    )
+}
+
+export default InputCalculatorForm;
