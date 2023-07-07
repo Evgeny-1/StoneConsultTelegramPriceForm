ef4eb588319010bbd2f54dff8f75eb1b79f528e2
Evgeny Larin <Larinevgenij@gmail.com>
2023-07-07 15:25:55 +0300

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
 import './Header.css';
+import logo from './Images/StoneConsult.png';
 
 const Header = () => {
     const {user, onClose} = useTelegram();
 
     return (
         <div className={'header'}>
+            <img src={logo} width="50" height="50" />
             StoneConsult
         </div>
     );
