ab1c38ec729f732732e251ac5e07869bb06ab40b
Evgeny Larin <Larinevgenij@gmail.com>
2023-07-07 16:46:54 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from "react";
+import React, {useState} from "react";
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
@@ -9,80 +9,80 @@ import Form from "./components/Form/Form";
 import CalculatorForm from "./components/InputCalculatorForm/InputCalculatorForm";
 
 function App() {
-  const {onToggleButton, tg} = useTelegram();
+    const {onToggleButton, tg} = useTelegram();
 
-  useEffect(() => {
-    tg.ready();
-  }, []);
+    useEffect(() => {
+        tg.ready();
+    }, []);
 
-  const handleSubmit = (e) => {
-      e.preventDefault();
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
 
-  return (
-      <div className="app">
-          <form onSubmit={handleSubmit}>
-          <h1>StoneConsult</h1>
-          <CalculatorForm placeholder = "Курс ЦБ"/>
+    return (
+        <div className="app">
+            <form onSubmit={handleSubmit}>
+                <h1>Калькулятор</h1>
+                <CalculatorForm placeholder="Курс ЦБ"/>
 
-          <CalculatorForm placeholder = "Валюта($/€)"/>
+                <CalculatorForm placeholder="Валюта($/€)"/>
 
-          <CalculatorForm placeholder = "Страна"/>
+                <CalculatorForm placeholder="Страна"/>
 
-          <CalculatorForm placeholder = "Цена завода за м2"/>
+                <CalculatorForm placeholder="Цена завода за м2"/>
 
-          <CalculatorForm placeholder = "Ширина"/>
-          <CalculatorForm placeholder = "Длина"/>
-          <CalculatorForm placeholder = "Толщина"/>
+                <CalculatorForm placeholder="Ширина"/>
+                <CalculatorForm placeholder="Длина"/>
+                <CalculatorForm placeholder="Толщина"/>
 
-          <CalculatorForm placeholder = "Плотность"/>
+                <CalculatorForm placeholder="Плотность"/>
 
-          <CalculatorForm placeholder = "Объем"/>
-          <CalculatorForm placeholder = "Тоннаж"/>
-          <CalculatorForm placeholder = "Вместимость контейнера"/>
-          <div>"Контейнеры (шт)"</div>
+                <CalculatorForm placeholder="Объем"/>
+                <CalculatorForm placeholder="Тоннаж"/>
+                <CalculatorForm placeholder="Вместимость контейнера"/>
+                <div> "Контейнеры (шт)"
+                </div>
 
-          <div>"Инвойс"</div>
+                <div>"Инвойс"</div>
 
-          <CalculatorForm placeholder = "Фрахт"/>
-          <CalculatorForm placeholder = "Прочее"/>
-          <CalculatorForm placeholder = "Пошлина"/>
-          <div>"Итого"</div>
+                <CalculatorForm placeholder="Фрахт"/>
+                <CalculatorForm placeholder="Прочее"/>
+                <CalculatorForm placeholder="Пошлина"/>
+                <div>"Итого"</div>
 
-          <CalculatorForm placeholder = "НДС до РФ"/>
-              <div>"Итого"</div>
+                <CalculatorForm placeholder="НДС до РФ"/>
+                <div>"Итого"</div>
 
-          <CalculatorForm placeholder = "Банковские расходы"/>
-                  <div>"Итого"</div>
+                <CalculatorForm placeholder="Банковские расходы"/>
+                <div>"Итого"</div>
 
-          <CalculatorForm placeholder = "Фрахт без НДС"/>
-          <CalculatorForm placeholder = "Прочее без НДС"/>
-          <CalculatorForm placeholder = "Фрахт с НДС"/>
-          <CalculatorForm placeholder = "Прочее с НДС"/>
+                <CalculatorForm placeholder="Фрахт без НДС"/>
+                <CalculatorForm placeholder="Прочее без НДС"/>
+                <CalculatorForm placeholder="Фрахт с НДС"/>
+                <CalculatorForm placeholder="Прочее с НДС"/>
 
-          <CalculatorForm placeholder = "НДС по РФ"/>
-          <div>"Итого"</div>
+                <CalculatorForm placeholder="НДС по РФ"/>
+                <div>"Итого"</div>
 
-          <CalculatorForm placeholder = "Наценка"/>
-          <div>"Итого"</div>
+                <CalculatorForm placeholder="Наценка"/>
+                <div>"Итого"</div>
 
-          <CalculatorForm placeholder = "Наименование камня($/€)"/>
+                <CalculatorForm placeholder="Наименование камня($/€)"/>
 
-          <div>"Итого по Заказу"</div>
-          <div>"Цена за м2($/€)"</div>
-          <div>"Цена за м2(руб)"</div>
-          <div>"Цена за шт($/€)"</div>
-          <div>"Цена за шт(руб)"</div>
+                <div>"Итого по Заказу"</div>
+                <div>"Цена за м2($/€)"</div>
+                <div>"Цена за м2(руб)"</div>
+                <div>"Цена за шт($/€)"</div>
+                <div>"Цена за шт(руб)"</div>
             </form>
+            {/*<Routes>*/}
 
-        {/*<Routes>*/}
+            {/*  <Route index element={<ProductList />}/>*/}
+            {/* <Route path={'form'} element={<Form />}/>*/}
 
-        {/*  <Route index element={<ProductList />}/>*/}
-        {/* <Route path={'form'} element={<Form />}/>*/}
-
-        {/*</Routes>*/}
-      </div>
-  );
+            {/*</Routes>*/}
+        </div>
+    );
 }
 
 export default App;
