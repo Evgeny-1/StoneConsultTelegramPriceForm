e97c9cda0b4ac652e8d563dbaca61773c29f869a
Evgeny Larin <Larinevgenij@gmail.com>
2023-07-12 16:42:47 +0300

    init

--- src/App.js
+++ src/App.js
@@ -10,27 +10,8 @@ import InputForm from "./components/InputForm/InputForm";
 
 function App() {
 
-    const [stone, setStone] = useState("None");
-    const handleChangeStone = (event) => {
-        setStone(event.target.value)
-    }
-
-    const [currency, setCurrency] = useState("None");
-    const handleChangeCurrency = (event) => {
-        setCurrency(event.target.value)
-    }
-
-    const [portOfShipment, setPortOfShipment] = useState("None");
-    const handleChangePortOfShipment = (event) => {
-        setPortOfShipment(event.target.value)
-    }
-
-    const [pointOfDelivery, setPointOfDelivery] = useState("None");
-    const handleChangePointOfDelivery = (event) => {
-        setPointOfDelivery(event.target.value)
-    }
-
     const {onToggleButton, tg} = useTelegram();
+
     useEffect(() => {
         tg.ready();
     }, []);
@@ -38,62 +19,7 @@ function App() {
     return (
         <div className={'container'}>
             <Header/>
-            <form>
-                <label><strong>Type of stone</strong></label>
-                <select value={stone} onChange={handleChangeStone}>
-                    <option value="None">None</option>
-                    <option value="Granite">Granite</option>
-                    <option value="Marble">Marble</option>
-                    <option value="Labrador">Labrador</option>
-                    <option value="Quartz">Quartz</option>
-                    <option value="Quartzite">Quartzite</option>
-                    <option value="Limestone">Limestone</option>
-                    <option value="Travertine">Travertine</option>
-                    <option value="Slate">Slate</option>
-                    <option value="Sandstone">Sandstone</option>
-                    <option value="Onyx">Onyx</option>
-                    <option value="Basalt">Basalt</option>
-                    <option value="Amethyst">Amethyst</option>
-                    <option value="Agate">Agate</option>
-                </select>
-                <label><strong>Geometry(Thick mm)</strong></label>
-                <InputForm placeholder="Thick, mm"/>
-                <label><strong>Quantity of goods in m2</strong></label>
-                <InputForm placeholder="Valume, m2"/>
-                <label><strong>Factory price per m2</strong></label>
-                <InputForm placeholder="Price"/>
-                <label><strong>Currency</strong></label>
-                <select value={currency} onChange={handleChangeCurrency}>
-                    <option value="None">None</option>
-                    <option value="$">$</option>
-                    <option value="€">€</option>
-                    <option value="₽">₽</option>
-                </select>
-                <label><strong>Port of shipment</strong></label>
-                <select value={portOfShipment} onChange={handleChangePortOfShipment}>
-                    <option value="None">None</option>
-                    <option value="Xiamen">Xiamen</option>
-                    <option value="Wuhan">Wuhan</option>
-                    <option value="Mundra">Mundra</option>
-                    <option value="Chennai">Chennai</option>
-                    <option value="Ho chi min">Ho chi min</option>
-                    <option value="Carrara">Carrara</option>
-                    <option value="Venezia">Venezia</option>
-                    <option value="Alicante">Alicante</option>
-                    <option value="Lisbon">Lisbon</option>
-                    <option value="Izmir">Izmir</option>
-                    <option value="Stambul">Stambul</option>
-                    <option value="Vitoria">Vitoria</option>
-                </select>
-                <label><strong>Point of delivery</strong></label>
-                <select value={pointOfDelivery} onChange={handleChangePointOfDelivery}>
-                    <option value="None">None</option>
-                    <option value="Vladikavkaz">Vladikavkaz</option>
-                    <option value="Novorossiysk">Novorossiysk</option>
-                    <option value="Saint-Petersburg">Saint-Petersburg</option>
-                    <option value="Moscow">Moscow</option>
-                </select>
-            </form>
+            <InputForm />
         </div>
     );
 }
--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -4,51 +4,158 @@ import {useTelegram} from "../../hooks/useTelegram";
 import "./InputForm.css"
 
 const InputForm = (props) => {
-    const [focused, setFocused] = useState(false);
-    const handleFocus = (e) => {
-        setFocused(true);
-    };
-
-    const { label, errorMessage, onChange, id, ...inputProps } = props;
 
     const {tg} = useTelegram();
 
+    const [stone, setStone] = useState("None");
+    const handleChangeStone = (event) => {
+        setStone(event.target.value)
+    }
 
-    const onSendData = useCallback(() => {
-        const data = {
-            focused
-        }
-        tg.sendData(JSON.stringify(data));
-    }, [focused])
+    const [thick, setThick] = useState('');
+    const handleChangeThick = (e) => {
+        setThick(e.target.value)
+    }
+
+    const [volume, setVolume] = useState('');
+    const handleChangeVolume = (e) => {
+        setVolume(e.target.value)
+    }
+
+    const [price, setPrice] = useState('');
+    const handleChangePrice = (e) => {
+        setPrice(e.target.value)
+    }
+
+    const [currency, setCurrency] = useState("None");
+    const handleChangeCurrency = (event) => {
+        setCurrency(event.target.value)
+    }
+
+    const [portOfShipment, setPortOfShipment] = useState("None");
+    const handleChangePortOfShipment = (event) => {
+        setPortOfShipment(event.target.value)
+    }
+
+    const [pointOfDelivery, setPointOfDelivery] = useState("None");
+    const handleChangePointOfDelivery = (event) => {
+        setPointOfDelivery(event.target.value)
+    }
+
+    {/* WORK WITH MAIN BUTTON ------> START */}
+    // useEffect(() => {
+    //     tg.onEvent('mainButtonClicked', onSendData)
+    //     return () => {
+    //         tg.offEvent('mainButtonClicked', onSendData)
+    //     }
+    // }, [onSendData])
 
     useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
-        return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
-
-    useEffect(() => {
-        if(!focused) {
+        if(stone !== "None"
+            || !thick
+            || !volume
+            || !price
+            || currency !== "None"
+            || portOfShipment !== "None"
+            || pointOfDelivery !== "None") {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
         }
-    }, [focused])
+    }, [stone, thick, volume, price, currency, portOfShipment, pointOfDelivery])
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Calculate'
+        })
+    }, [])
+    {/* WORK WITH MAIN BUTTON ------> END */}
+
+    {/* SEND JSON FILE ON SERVER APP ------> START */}
+    // const onSendData = useCallback(() => {
+    //     const data = {
+    //         stone,
+    //         currency,
+    //         portOfShipment,
+    //         pointOfDelivery
+    //     }
+    //     tg.sendData(JSON.stringify(data));
+    // }, [stone, currency, portOfShipment, pointOfDelivery])
+    {/* SEND JSON FILE ON SERVER APP ------> START */}
 
     return (
-        <div className="formInput">
-            <label>{label}</label>
-            <input
-                {...inputProps}
-                onChange={onChange}
-                onBlur={handleFocus}
-                onFocus={() =>
-                    inputProps.name === "confirmPassword" && setFocused(true)
-                }
-                focused={focused.toString()}
-            />
-            <span>{errorMessage}</span>
+        <div>
+            <form>
+                <label><strong>Type of stone</strong></label>
+                <select value={stone} onChange={handleChangeStone}>
+                    <option value="None">None</option>
+                    <option value="Granite">Granite</option>
+                    <option value="Marble">Marble</option>
+                    <option value="Labrador">Labrador</option>
+                    <option value="Quartz">Quartz</option>
+                    <option value="Quartzite">Quartzite</option>
+                    <option value="Limestone">Limestone</option>
+                    <option value="Travertine">Travertine</option>
+                    <option value="Slate">Slate</option>
+                    <option value="Sandstone">Sandstone</option>
+                    <option value="Onyx">Onyx</option>
+                    <option value="Basalt">Basalt</option>
+                    <option value="Amethyst">Amethyst</option>
+                    <option value="Agate">Agate</option>
+                </select>
+                <label><strong>Geometry(Thick mm)</strong></label>
+                <input
+                    type="text"
+                    placeholder={'Thick, mm'}
+                    value={thick}
+                    onChange={handleChangeThick}
+                />
+                <label><strong>Quantity of goods in m2</strong></label>
+                <input
+                    type="text"
+                    placeholder={'Volume, m2'}
+                    value={volume}
+                    onChange={handleChangeVolume}
+                />
+                <label><strong>Factory price per m2</strong></label>
+                <input
+                    type="text"
+                    placeholder={'Price'}
+                    value={price}
+                    onChange={handleChangePrice}
+                />
+                <label><strong>Currency</strong></label>
+                <select value={currency} onChange={handleChangeCurrency}>
+                    <option value="None">None</option>
+                    <option value="$">$</option>
+                    <option value="€">€</option>
+                    <option value="₽">₽</option>
+                </select>
+                <label><strong>Port of shipment</strong></label>
+                <select value={portOfShipment} onChange={handleChangePortOfShipment}>
+                    <option value="None">None</option>
+                    <option value="Xiamen">Xiamen</option>
+                    <option value="Wuhan">Wuhan</option>
+                    <option value="Mundra">Mundra</option>
+                    <option value="Chennai">Chennai</option>
+                    <option value="Ho chi min">Ho chi min</option>
+                    <option value="Carrara">Carrara</option>
+                    <option value="Venezia">Venezia</option>
+                    <option value="Alicante">Alicante</option>
+                    <option value="Lisbon">Lisbon</option>
+                    <option value="Izmir">Izmir</option>
+                    <option value="Stambul">Stambul</option>
+                    <option value="Vitoria">Vitoria</option>
+                </select>
+                <label><strong>Point of delivery</strong></label>
+                <select value={pointOfDelivery} onChange={handleChangePointOfDelivery}>
+                    <option value="None">None</option>
+                    <option value="Vladikavkaz">Vladikavkaz</option>
+                    <option value="Novorossiysk">Novorossiysk</option>
+                    <option value="Saint-Petersburg">Saint-Petersburg</option>
+                    <option value="Moscow">Moscow</option>
+                </select>
+            </form>
         </div>
     );
 };
