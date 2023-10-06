a3764459320c109b8f8672a0d12c76f667b8514b
Evgeny Larin <Larinevgenij@gmail.com>
2023-10-06 11:56:21 +0300

    Update

--- src/components/PriceForm/PriceForm.jsx
+++ src/components/PriceForm/PriceForm.jsx
@@ -13,7 +13,7 @@ const PriceForm = (props) => {
         setStoneType(event.target.value)
     }
 
-    const [stoneName, setStoneName] = useState("");
+    const [stoneName, setStoneName] = useState(tg.username);
     const handleChangeStoneName = (event) => {
         setStoneName(event.target.value)
     }
