817d818fe2a22d70c3c76682099c4b0604fa6dc9
Evgeny Larin <Larinevgenij@gmail.com>
2023-10-27 13:44:37 +0300

    Update

--- src/hooks/Variables.jsx
+++ src/hooks/Variables.jsx
@@ -1,4 +1,4 @@
 export const variables={
     API_URL:'https://localhost:7124/api/',
-    API_REMOTE_URL:'https://www.stoneconsult-service.ru/api/'
+    API_REMOTE_URL:'https://www.stoneconsult-service.ru/telegramPriceBot/'
 }
